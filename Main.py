2e766cee1307778de09bdaeeb9f45d5b1d52c7a2
isoBello <isabellamfreitas2@gmail.com>
2020-11-02 12:23:36 -0300

    Evolving the problem

--- Main.py
+++ Main.py
@@ -1,40 +1,62 @@
 # -*- coding: <UTF-8> -*-
 # !/usr/bin/env pypy
 import sys
+import PG
 
-data = {}
-FILES = []
 args = 0
+data = {}
 
 
 def read(index):
     global args, data
-    with open(FILES[index], "r") as archive:
-        lines = archive.readlines()
-        for line in lines:
-            values = line.split(",")
-            for v in values[:-1]:
-                v = float(v)
-                temp.append(v)
-            args = len(temp)
-            data[float(values[-1])] = temp
-            temp = []
-    archive.close()
+    temp = []
+    try:
+        with open(sys.argv[index], "r") as archive:
+            lines = archive.readlines()
+            for line in lines:
+                values = line.split(",")
+                for v in values[:-1]:
+                    v = float(v)
+                    temp.append(v)
+                args = len(temp)
+                data[float(values[-1])] = temp
+                temp = []
+        archive.close()
+    except ValueError:
+        print("Nothing to be done here! Moving on...")
 
 
 def main():
-    for i in range(0, 5):
-        FILES.append(sys.argv[i])
+    PG.datasets()
 
 
-def data():
+def get_data():
     global data
     return data
 
 
-def args():
+def get_args():
     global args
     return args
+
+
+def statistics(pop, log, hof, df):
+    # Do stuff
+
+    df.to_csv(sys.argv[6], index=True)  # Write the csv file
+
+    # Write LOG
+    with open(sys.argv[7], "w") as archive:
+        archive.write("Population: " + '\n')
+        for p in pop:
+            archive.write(str(p) + '\n')
+        archive.write('\n')
+        archive.write(str(log) + '\n')
+        archive.write('\n')
+        for h in hof:
+            archive.write("Best individual: " + str(h) + '\n')
+            archive.write("Fitness: " + str(h.fitness) + '\n')
+    archive.close()
 
 
 if __name__ == "__main__":
